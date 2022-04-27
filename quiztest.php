628c34853c3e2525e60fec63226db2b7af151e9b
IestynGreen <20207508@hope.ac.uk>
2022-04-27 15:17:09 +0100

    Update quiztest.php

--- quiztest.php
+++ quiztest.php
@@ -3,12 +3,8 @@
     $database_name = "quiz";
     $con = mysqli_connect("localhost", "root", "", $database_name);
     $QuestionNum = 1;
-    $AnswerNum = 1;
+    $AnswerNum = 0;
     $Score = 0;
-
-
-
-
     ?>
 
 <html lang="en" dir="ltr">
@@ -22,50 +18,65 @@
     $query = "SELECT * FROM quizqa ORDER BY QuestionID ASC ";
     $result = mysqli_query($con, $query);
 
+    $CorrectArray = [];
+    $ChoiceArray = [];
+
+    if(mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
 
 
-
-
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_array($result)){
-
-
-
-            $ans2 =$ans=array($row['Answer1'],$row['Answer2'],$row['Answer3'],$row['Answer4']);
+            $ans = array($row['Answer1'], $row['Answer2'], $row['Answer3'], $row['Answer4']);
             shuffle($ans);
+            $name = 'Button+'.strval($QuestionNum);
+            $val = 'test+'.strval($AnswerNum);
 
-            echo '<form action="quiztest.php" method="POST">';
+            ?>
 
-            echo "<p>Question.$QuestionNum</p>";
-            echo $row["Question"];
-            echo "<br>";
-            echo "<p>Select the correct answer below </p>";
-            foreach($ans as $choice) {
-                echo "<input type='radio' name=string($AnswerNum)>".$choice."</input><br>";
+            <form action="quiztest.php" method="POST">
 
+            <?php echo '<p>Question '.$QuestionNum.'</p>';
+            echo $row["Question"]; ?>
+            <br>
+            <p>Select the correct answer below </p>
+            <?php foreach ($ans as $choice) { ?>
+                    <input type=radio name=<?php echo $name ?> value=<?php echo $choice?>>
+                <label for=<?php echo $name ?>> <?php echo $choice ?><br></label>
+                <?php
+
+                $AnswerNum += 1;
+                $val = 'test+'.strval($AnswerNum);
             }
+
+            if (isset($_POST[$name])) {
+                // Show the radio button value, i.e. which one was checked when the form was sent
+                $x = $_POST[$name];
+                $ChoiceArray[$QuestionNum] = $x;
+                array_push($ChoiceArray, $x);
+            }
+            else{
+                $ChoiceArray[$QuestionNum] = "";
+            }
+            $CorrectArray[$QuestionNum] = $row['Answer1'];
             $QuestionNum += 1;
-
         }
-            echo '<button type="submit" name="butt"> Finish Quiz </button>';
-            echo $row;
-
-                if (isset($_POST['butt'])) {
-                    for ($i = 0; $i < $AnswerNum; $i++) {
-                        echo ($row);
-                        if ($i == isset($ans2[2])){
-                            echo '<script>alert("Correct")</script>';
-                        }
-                    }
-
-                }
-
 
     }
 
 
+echo '<button type="submit" name="butt" value="Submit"> Finish Quiz </button>';
+echo'</form>';
+    if (isset($_POST["butt"])) {
+        for ($i = 1; $i < $QuestionNum; $i++) {
+            //echo $CorrectArray[$i];
+                if ($ChoiceArray[$i] == $CorrectArray[$i]) {
+                    $Score += 1;
+                }
+        }
+        echo '<script>alert(' . $Score . ')</script>';
+    }
 
     ?>
+
 <br>
 
 </body>
